56ffe9b0aaa4789a73ac4a9335832f58d374049d
Adya_MP <prawira701am@gmail.com>
2025-06-09 22:31:15 +0700

    Initial Commit

---
+++ Bebas/BebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BebasApp.swift
+//  Bebas
+//
+//  Created by Adya Muhammad Prawira on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BebasTests/BebasTests.swift
@@ -0,0 +1,17 @@
+//
+//  BebasTests.swift
+//  BebasTests
+//
+//  Created by Adya Muhammad Prawira on 09/06/25.
+//
+
+import Testing
+@testable import Bebas
+
+struct BebasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
