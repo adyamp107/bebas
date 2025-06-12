938eb8eb5d1549e9b682f5d1f57d797023bd100c
Adya_MP <prawira701am@gmail.com>
2025-06-12 15:20:18 +0700

    Card Kamus

---
+++ Bebas/View/DictionaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DictionaryView.swift
+//  Bebas
+//
+//  Created by Adya Muhammad Prawira on 12/06/25.
+//
+
+import SwiftUI
+
+struct DictionaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DictionaryView()
+}
