c20840818e6e45aeb8704939fe52a7899c1ccb79
Adya_MP <prawira701am@gmail.com>
2025-06-13 22:40:21 +0700

    bagian belajar (pilih kata & onboarding)

---
+++ Bebas/View/LearnOnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnOnBoardingView.swift
+//  Bebas
+//
+//  Created by Adya Muhammad Prawira on 13/06/25.
+//
+
+import SwiftUI
+
+struct LearnOnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnOnBoardingView()
+}
